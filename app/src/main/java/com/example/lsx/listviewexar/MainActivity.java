10aee62e2a25c1ceadda9785e2b667537f13aa3f
hxj <hxj@163.com>
2016-07-24 10:55:09 +0800

    listview只有文字

--- app/src/main/java/com/example/lsx/listviewexar/MainActivity.java
+++ app/src/main/java/com/example/lsx/listviewexar/MainActivity.java
@@ -2,12 +2,22 @@ package com.example.lsx.listviewexar;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
+    private ListView mListView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mListView = (ListView) findViewById(R.id.activity_main_list_view);
+        String[] data = {"apple","banana","pear","orange","apple","banana",
+                "pear","orange","apple","banana","pear","orange","banana","pear","orange"};
+        ArrayAdapter<String> adaper = new ArrayAdapter<String>(this,
+                        android.R.layout.simple_list_item_1,data);
+        mListView.setAdapter(adaper);
     }
 }
